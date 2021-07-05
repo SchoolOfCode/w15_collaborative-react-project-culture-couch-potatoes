5fea7f8a1f3321d7a163d8137ef2e0ddd3e01d5e
gschandan <gsc128@gmail.com>
2021-07-05 01:18:16 +0000

    automated exhibit item placement and expanding size of the museum with loops to create the displays and some constants declared in app.js to scale

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
- import { OrbitControls, Stars } from "@react-three/drei";
-// import { PointerLockControls } from "@react-three/drei";
+ import { PointerLockControls, Stars, FlyControls, OrbitControls } from "@react-three/drei";
 import Museum from "./components/museum";
 import Exhibit from "./components/exhibits";
 import Floor from "./components/floor";
@@ -14,55 +13,57 @@ import squid from "./objects/squid.png"
 import "./App.css";
 
 const MUS_WIDTH = 50;
-const MUS_HEIGHT = 10;
+const MUS_HEIGHT = 12;
 const MUS_POSITION = [0,0,0]
 const FLOOR_SIZE = 10;
-const TOTAL_EXHIB = 4;
+const EXHIBITS = [mona, boromir, godzilla, squid]
+const EXHIBIT_PROPS = [
+  [mona,{size:[3.5,5],text:"Mona Lisa | 300 BC"}],
+  [boromir,{size:[8,5],text:"Boromir's Sorrow | 903 AD"}],
+  [godzilla,{size:[7,5],text:"Starry Night | 123 AD"}],
+  [squid,{size:[5,5],text:"S. Ward | Unknown"}]
+];
 
 function App() {
+
+  const exhibitItems = []
+  for (let i = 0, j = -MUS_WIDTH/2; i < EXHIBITS.length; i++, j+= (MUS_WIDTH/(EXHIBITS.length-1))){
+    exhibitItems.push(
+      <Exhibit 
+          position={[j, 1, 0.1]} 
+          rotation={[0,0,0]} 
+          size={EXHIBIT_PROPS[i][1].size} 
+          text={EXHIBIT_PROPS[i][1].text}
+          image={EXHIBIT_PROPS[i][0]}
+      />
+    );
+  }
+
   return (
     <>
     
-    <Canvas camera= {{position:[0,0,15],fov:75}}>
+    <Canvas 
+      style={{height:"100vh", width:"100vw"}}
+      dpr={Math.max(window.devicePixelRatio, 2)} 
+      resize={{scroll: false}} 
+      camera={{position:[MUS_POSITION[0],MUS_POSITION[1],FLOOR_SIZE+5],fov:75}}
+      >
       <Suspense fallback={null}>
 
         <ambientLight intensity={0.9} />
 
         <Museum position={MUS_POSITION} width={MUS_WIDTH} height={MUS_HEIGHT} floorSize={FLOOR_SIZE}/>
         <Floor position={MUS_POSITION} width={MUS_WIDTH} height={MUS_HEIGHT} floorSize={FLOOR_SIZE}/>
+  
+        {exhibitItems}
 
-
-        <Exhibit 
-        position={[-MUS_WIDTH/2 + 0.1, 0, FLOOR_SIZE/2]} 
-        rotation={[0,Math.PI/2,0]} 
-        size={[5,5]} 
-        text={"Starry Night | 123 AD"}
-        image={godzilla}
-        />
-
-        <Exhibit 
-        position={[-12, 1, 0.2]} 
-        size={[3.5,5]} 
-        text={"Mona Lisa | 300 BC"}
-        image={mona}
-        />
-
-        <Exhibit 
-        position={[12, 1, 0.2]}
-        size={[9,5]} 
-        text={"Boromir's Sorrow | 903 AD"}
-        image={boromir}
-        />
-
-        <Exhibit 
-        position={[MUS_WIDTH/2 - 0.1, 0, FLOOR_SIZE/2]} 
-        rotation={[0,-Math.PI/2,0]}
-        size={[4,4]} 
-        text={"S. Ward | Unknown"}
-        image={squid}
-        />
-
+        {/* <FlyControls 
+          movementSpeed={10}
+          rollSpeed={0.8}
+          dragToLook={true}
+        /> */}
         <OrbitControls />
+        {/* <PointerLockControls /> */}
         <Stars />
 
       </Suspense>
--- src/components/museum/index.js
+++ src/components/museum/index.js
@@ -7,16 +7,16 @@ function Museum({position,width,height,floorSize}) {
     <>
     {/* BACK WALL */}
       <mesh position={position}>
-        <planeBufferGeometry attach="geometry" args={[width,height]} />
+        <planeBufferGeometry attach="geometry" args={[width+floorSize,height]} />
         <meshLambertMaterial attach="material" color= {"#002a36"} side={DoubleSide}/>
       </mesh>
     {/* LEFT WALL */}
-      <mesh position={[-width/2, 0, floorSize/2]} rotation={[0,Math.PI/2,0]}>
+      <mesh position={[-(width+floorSize)/2, 0, floorSize/2]} rotation={[0,Math.PI/2,0]}>
         <planeBufferGeometry attach="geometry" args={[floorSize,height]} />
         <meshLambertMaterial attach="material" color= {"#002a36"} side={DoubleSide}/>
       </mesh>
     {/* RIGHT WALL */}
-      <mesh position={[width/2, 0, floorSize/2]} rotation={[0,-Math.PI/2,0]}>
+      <mesh position={[(width+floorSize)/2, 0, floorSize/2]} rotation={[0,-Math.PI/2,0]}>
         <planeBufferGeometry attach="geometry" args={[floorSize,height]} />
         <meshLambertMaterial attach="material" color= {"#002a36"} side={DoubleSide}/>
       </mesh>
