aa6d4e6f6850f163e39b3639b33904f849c75738
gschandan <gsc128@gmail.com>
2021-07-07 11:04:04 +0000

    working overlay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Suspense, useRef } from "react";
-import { Canvas, useThree} from "@react-three/fiber";
-import { OrbitControls, Html} from "@react-three/drei";
+import React, { Suspense} from "react";
+import { Canvas} from "@react-three/fiber";
+import { OrbitControls} from "@react-three/drei";
 
 import Museum from "./components/museum";
 import Exhibit from "./components/exhibits";
 import Floor from "./components/floor";
+
+import mona from "./objects/mona.jpg"
+import boromir from "./objects/boromir.jpg"
+import godzilla from "./objects/van_gough.jpg"
+import squid from "./objects/squid.png"
 
 import "./App.css";
 
@@ -12,9 +17,52 @@ const MUS_WIDTH = 50;
 const MUS_HEIGHT = 12;
 const MUS_POSITION = [0,0,0]
 const FLOOR_SIZE = 10;
+const EXHIBITS = [mona, boromir, godzilla, squid]
+const EXHIBIT_PROPS = [
+  [mona,{
+    id:1,  
+    size:[3.5,5], 
+    text:"Mona Lisa | 300 BC", 
+    description:"Welcome to the virtual history and science museum."}],
+  [boromir, {
+      id:2,  
+      size:[8,5],   
+      text:"Boromir's Warning | 903 AD",
+      description:"blah blah blah."}],
+  [godzilla,{
+      id:3,  
+      size:[7,5],   
+      text:"Starry Night | 123 AD", 
+      description:"testing testing testing"}],
+  [squid,{
+      id:4,  
+      size:[5,5],   
+      text:"S. Ward | Unknown", 
+      description:"12345647369239487368673984789374892374."}]
+];
 
+const setupExhibitItems = () =>{
+  const exhibitItems = []
+
+  for (let i = 0, j = -MUS_WIDTH/2; i < EXHIBITS.length; i++, j+= Math.floor((MUS_WIDTH/(EXHIBITS.length-1)))){
+    exhibitItems.push(
+        <Exhibit 
+            key={EXHIBIT_PROPS[i][1].id}
+            position={[j, 1, 0.1]} 
+            size={EXHIBIT_PROPS[i][1].size} 
+            text={EXHIBIT_PROPS[i][1].text}
+            image={EXHIBIT_PROPS[i][0]}
+            museumParams={[MUS_WIDTH,MUS_HEIGHT,FLOOR_SIZE]}
+            description = {EXHIBIT_PROPS[i][1].description}
+        />
+    );
+  }
+  return exhibitItems;
+}
 
 function App() {
+
+  const items = setupExhibitItems();
 
   return (
     <>
@@ -27,7 +75,7 @@ function App() {
             <ambientLight intensity={0.6} />
             <Museum position={MUS_POSITION} width={MUS_WIDTH} height={MUS_HEIGHT} floorSize={FLOOR_SIZE}/>
             <Floor position={MUS_POSITION} width={MUS_WIDTH} height={MUS_HEIGHT} floorSize={FLOOR_SIZE}/>
-            <Exhibit width={MUS_WIDTH} height={MUS_HEIGHT} depth={FLOOR_SIZE}/>
+            {items}
             <OrbitControls 
               minDistance={5} maxDistance={FLOOR_SIZE-1}
               minAzimuthAngle ={-Math.PI/2.1} maxAzimuthAngle={Math.PI/2.1}
--- src/components/exhibits/index.js
+++ src/components/exhibits/index.js
@@ -2,68 +2,11 @@ import React, { useRef, useState, useMemo } from "react";
 import { Text, useTexture, Html } from "@react-three/drei";
 import { DoubleSide, SpotLight} from 'three'
 
-import mona from "../../objects/mona.jpg"
-import boromir from "../../objects/boromir.jpg"
-import godzilla from "../../objects/van_gough.jpg"
-import squid from "../../objects/squid.png"
-
-const EXHIBITS = [mona, boromir, godzilla, squid]
-const EXHIBIT_PROPS = [
-  [mona,{
-    id:1,  
-    size:[3.5,5], 
-    text:"Mona Lisa | 300 BC", 
-    description:"Welcome to the virtual history and science museum."}],
-  [boromir, 
-    {id:2,  
-      size:[8,5],   
-      text:"Boromir's Warning | 903 AD", 
-      description:"blah blah blah."}],
-  [godzilla,
-    {id:3,  
-      size:[7,5],   
-      text:"Starry Night | 123 AD", 
-      description:"testing testing testing"}],
-  [squid,   
-    {id:4,  
-      size:[5,5],   
-      text:"S. Ward | Unknown", 
-      description:"12345647369239487368673984789374892374."}]
-];
-
 const helperText = "Welcome to the virtual history and science museum. An interactive virtual tour that captures some of the most fascinating moments inhistory. To explore the museum use your mouse left to right. Click on exhibits of interest to find out some historic facts. We hope you enjoythe museum!."
-
-const exhibitItems = []
-  for (let i = 0, j = -MUS_WIDTH/2; i < EXHIBITS.length; i++, j+= Math.floor((MUS_WIDTH/(EXHIBITS.length-1)))){
-    exhibitItems.push(
-      <>
-        <Exhibit 
-            key={EXHIBIT_PROPS[i][1].id}
-            position={[j, 1, 0.1]} 
-            rotation={[0,0,0]} 
-            size={EXHIBIT_PROPS[i][1].size} 
-            text={EXHIBIT_PROPS[i][1].text}
-            image={EXHIBIT_PROPS[i][0]}
-            museumParams={[MUS_WIDTH,MUS_HEIGHT,FLOOR_SIZE]}
-        />
-        <Html 
-          position = {[j, 1, 2]}
-          as='div'
-          className="description"
-          style={{
-            color: "white",
-            backgroundColor: "rgba(0,0,0,0.2)",
-            transition: 'all 0.5s',
-          }}>
-            <p> {EXHIBIT_PROPS[i][1].description} </p>
-          </Html>
-      </>
-    );
-  }
-
 
 
 function Exhibit(props) {
+
   const mesh = useRef();
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
@@ -79,16 +22,27 @@ function Exhibit(props) {
         onClick={(e) => setActive(!active)}
         onPointerOver={(e) => setHover(true)}
         onPointerOut={(e) => setHover(false)}
+
         >
         <planeBufferGeometry attach="geometry" args={props.size} />
         <meshStandardMaterial
-          opacity={hovered ? 1 : 0.95}
           attach="material"
           map={imgTexture}
           side={DoubleSide}
         />
-      <Overlay active={active} text={helperText} position = {props.position}/>
       </mesh>
+      <Html 
+        position = {[props.position[0], 1, 1]}
+        as='div'
+        className="description"
+        style={{
+          color: "white",
+          backgroundColor: "rgba(0,0,0,0.4)",
+          transition: 'all 0.5s',
+          opacity: active ? 1 : 0,
+        }}>
+          <p> {props.description}</p>
+        </Html>
       <primitive object={spot} 
         position={[props.position[0],5, props.museumParams[2]]} 
         intensity={0.8}
@@ -112,33 +66,45 @@ function Exhibit(props) {
 
   );
 }
+// }
+{/* <Html 
+position = {[j, 1, 2]}
+as='div'
+className="description"
+style={{
+  color: "white",
+  backgroundColor: "rgba(0,0,0,0.2)",
+  transition: 'all 0.5s',
+}}>
+  <p> {EXHIBIT_PROPS[i][1].description} </p>
+</Html> */}
 
-function Overlay({active, text, position}){
-  const [hidden, set] = useState(false)
+// function Overlay({active, text, position}){
+//   const [hidden, set] = useState(false)
     
-  return(
-        // <Html className={active? null: "hidden"} as='div' center="true" style={{color:"white", backgroundColor: "black", width:"100vw",height: "100vh"}}>
-        <Html 
-        as='div'
-        className="description"
-        position = {position}
-        occlude
-        onOcclude={set}
-        style={{
-          display: "grid",
-          color: "white",
-          backgroundColor: "rgba(0,0,0,0.2)",
-          transition: 'all 0.5s',
-          opacity: active ? 1 : 0,
-        }}>
-          <p 
-            style={{}}
-            >
-            {text}
-          </p>
-        </Html>
+//   return(
+//         // <Html className={active? null: "hidden"} as='div' center="true" style={{color:"white", backgroundColor: "black", width:"100vw",height: "100vh"}}>
+//         <Html 
+//         as='div'
+//         className="description"
+//         position = {position}
+//         occlude
+//         onOcclude={set}
+//         style={{
+//           display: "grid",
+//           color: "white",
+//           backgroundColor: "rgba(0,0,0,0.2)",
+//           transition: 'all 0.5s',
+//           opacity: active ? 1 : 0,
+//         }}>
+//           <p 
+//             style={{}}
+//             >
+//             {text}
+//           </p>
+//         </Html>
 
-  )
-}
+//   )
+// }
   
 export default Exhibit;
