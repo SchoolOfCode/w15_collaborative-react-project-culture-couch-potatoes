14b9d7ac490bf3faa94b37652a8694bef5decc39
gschandan <gsc128@gmail.com>
2021-07-07 10:32:39 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Suspense, useRef } from "react";
 import { Canvas, useThree} from "@react-three/fiber";
-import { OrbitControls} from "@react-three/drei";
+import { OrbitControls, Html} from "@react-three/drei";
 
 import Museum from "./components/museum";
 import Exhibit from "./components/exhibits";
 import Floor from "./components/floor";
-
-import mona from "./objects/mona.jpg"
-import boromir from "./objects/boromir.jpg"
-import godzilla from "./objects/van_gough.jpg"
-import squid from "./objects/squid.png"
 
 import "./App.css";
 
@@ -17,53 +12,9 @@ const MUS_WIDTH = 50;
 const MUS_HEIGHT = 12;
 const MUS_POSITION = [0,0,0]
 const FLOOR_SIZE = 10;
-const EXHIBITS = [mona, boromir, godzilla, squid]
-const EXHIBIT_PROPS = [
-  [mona,    {id:1,  size:[3.5,5], text:"Mona Lisa | 300 BC"}],
-  [boromir, {id:2,  size:[8,5],   text:"Boromir's Warning | 903 AD"}],
-  [godzilla,{id:3,  size:[7,5],   text:"Starry Night | 123 AD"}],
-  [squid,   {id:4,  size:[5,5],   text:"S. Ward | Unknown"}]
-];
-
-const setupExhibitItems = () =>{
-  const exhibitItems = []
-  for (let i = 0, j = -MUS_WIDTH/2; i < EXHIBITS.length; i++, j+= Math.floor((MUS_WIDTH/(EXHIBITS.length-1)))){
-    exhibitItems.push(
-      <Exhibit 
-          key={EXHIBIT_PROPS[i][1].id}
-          position={[j, 1, 0.1]} 
-          rotation={[0,0,0]} 
-          size={EXHIBIT_PROPS[i][1].size} 
-          text={EXHIBIT_PROPS[i][1].text}
-          image={EXHIBIT_PROPS[i][0]}
-          museumParams={[MUS_WIDTH,MUS_HEIGHT,FLOOR_SIZE]}
-      />
-    );
-  }
-  return exhibitItems;
-}
-
-// const Camera = ({position=[MUS_POSITION[0],MUS_POSITION[1],FLOOR_SIZE+5], fov=75, items}) =>{
-//   const cam = useRef()
-  
-//   return (
-//     <>
-//     <PerspectiveCamera 
-//       ref={cam} 
-//       makeDefault 
-//       position={position} 
-//       fov={fov}
-//     />
-    
-//   </>
-//   )
-// }
 
 
 function App() {
-  
-  const viewport = useThree();
-  const items = setupExhibitItems();
 
   return (
     <>
@@ -71,30 +22,21 @@ function App() {
       style={{height:"100vh", width:"100vw"}}
       dpr={Math.max(window.devicePixelRatio, 2)} 
       resize={{scroll: false}}
-      >
+      camera={{position:[MUS_POSITION[0],MUS_POSITION[1],FLOOR_SIZE+5], fov:95}}>
+        <Suspense fallback={null}>
+            <ambientLight intensity={0.6} />
+            <Museum position={MUS_POSITION} width={MUS_WIDTH} height={MUS_HEIGHT} floorSize={FLOOR_SIZE}/>
+            <Floor position={MUS_POSITION} width={MUS_WIDTH} height={MUS_HEIGHT} floorSize={FLOOR_SIZE}/>
+            <Exhibit width={MUS_WIDTH} height={MUS_HEIGHT} depth={FLOOR_SIZE}/>
+            <OrbitControls 
+              minDistance={5} maxDistance={FLOOR_SIZE-1}
+              minAzimuthAngle ={-Math.PI/2.1} maxAzimuthAngle={Math.PI/2.1}
+              minPolarAngle={1} maxPolarAngle={Math.PI/1.5}
+              keyPanSpeed={10}>
+            </OrbitControls>
 
-      <Suspense fallback={null}>
-
-        <ambientLight intensity={0.6} />
-
-        <Museum position={MUS_POSITION} width={MUS_WIDTH} height={MUS_HEIGHT} floorSize={FLOOR_SIZE}/>
-        <Floor position={MUS_POSITION} width={MUS_WIDTH} height={MUS_HEIGHT} floorSize={FLOOR_SIZE}/>
-        {items}
-        {console.log(viewport)}
-        <OrbitControls 
-          minDistance={5}
-          maxDistance={FLOOR_SIZE-1}
-          minAzimuthAngle ={-Math.PI/2.1}
-          maxAzimuthAngle={Math.PI/2.1}
-          minPolarAngle={1}
-          maxPolarAngle={Math.PI/1.5}
-          keyPanSpeed={10}
-        >
-        </OrbitControls>
-
-      </Suspense>
+        </Suspense>
     </Canvas>
-    
     </>
   );
 }
--- src/components/exhibits/index.js
+++ src/components/exhibits/index.js
@@ -2,8 +2,66 @@ import React, { useRef, useState, useMemo } from "react";
 import { Text, useTexture, Html } from "@react-three/drei";
 import { DoubleSide, SpotLight} from 'three'
 
+import mona from "../../objects/mona.jpg"
+import boromir from "../../objects/boromir.jpg"
+import godzilla from "../../objects/van_gough.jpg"
+import squid from "../../objects/squid.png"
+
+const EXHIBITS = [mona, boromir, godzilla, squid]
+const EXHIBIT_PROPS = [
+  [mona,{
+    id:1,  
+    size:[3.5,5], 
+    text:"Mona Lisa | 300 BC", 
+    description:"Welcome to the virtual history and science museum."}],
+  [boromir, 
+    {id:2,  
+      size:[8,5],   
+      text:"Boromir's Warning | 903 AD", 
+      description:"blah blah blah."}],
+  [godzilla,
+    {id:3,  
+      size:[7,5],   
+      text:"Starry Night | 123 AD", 
+      description:"testing testing testing"}],
+  [squid,   
+    {id:4,  
+      size:[5,5],   
+      text:"S. Ward | Unknown", 
+      description:"12345647369239487368673984789374892374."}]
+];
 
 const helperText = "Welcome to the virtual history and science museum. An interactive virtual tour that captures some of the most fascinating moments inhistory. To explore the museum use your mouse left to right. Click on exhibits of interest to find out some historic facts. We hope you enjoythe museum!."
+
+const exhibitItems = []
+  for (let i = 0, j = -MUS_WIDTH/2; i < EXHIBITS.length; i++, j+= Math.floor((MUS_WIDTH/(EXHIBITS.length-1)))){
+    exhibitItems.push(
+      <>
+        <Exhibit 
+            key={EXHIBIT_PROPS[i][1].id}
+            position={[j, 1, 0.1]} 
+            rotation={[0,0,0]} 
+            size={EXHIBIT_PROPS[i][1].size} 
+            text={EXHIBIT_PROPS[i][1].text}
+            image={EXHIBIT_PROPS[i][0]}
+            museumParams={[MUS_WIDTH,MUS_HEIGHT,FLOOR_SIZE]}
+        />
+        <Html 
+          position = {[j, 1, 2]}
+          as='div'
+          className="description"
+          style={{
+            color: "white",
+            backgroundColor: "rgba(0,0,0,0.2)",
+            transition: 'all 0.5s',
+          }}>
+            <p> {EXHIBIT_PROPS[i][1].description} </p>
+          </Html>
+      </>
+    );
+  }
+
+
 
 function Exhibit(props) {
   const mesh = useRef();
@@ -57,9 +115,7 @@ function Exhibit(props) {
 
 function Overlay({active, text, position}){
   const [hidden, set] = useState(false)
-  
-  console.log(position)
-  
+    
   return(
         // <Html className={active? null: "hidden"} as='div' center="true" style={{color:"white", backgroundColor: "black", width:"100vw",height: "100vh"}}>
         <Html 
@@ -74,7 +130,6 @@ function Overlay({active, text, position}){
           backgroundColor: "rgba(0,0,0,0.2)",
           transition: 'all 0.5s',
           opacity: active ? 1 : 0,
-          transform: `scale(${active ? 1 : 0.5})`,
         }}>
           <p 
             style={{}}
