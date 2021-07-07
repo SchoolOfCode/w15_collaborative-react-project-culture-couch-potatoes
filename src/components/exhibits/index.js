ec744d44e6ec8a37f3c79244f7fc2ed9ccb3d368
gschandan <gsc128@gmail.com>
2021-07-07 14:00:19 +0000

    fixed centering of overlay

--- src/components/exhibits/index.js
+++ src/components/exhibits/index.js
@@ -5,34 +5,31 @@ import { DoubleSide, SpotLight} from 'three'
 const helperText = "Welcome to the virtual history and science museum. An interactive virtual tour that captures some of the most fascinating moments inhistory. To explore the museum use your mouse left to right. Click on exhibits of interest to find out some historic facts. We hope you enjoythe museum!."
 
 
-function Exhibit(props) {
+function Exhibit({position,size,text,image,altText,museumParams,description}) {
 
   const mesh = useRef();
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  const imgTexture = useTexture(props.image);
+  const imgTexture = useTexture(image);
   const spot = useMemo(() => new SpotLight(0xffffff), [])
 
   return (
     <>
       <mesh
-        {...props}
+        position={position}
         ref={mesh}
         scale={hovered ? 1.1 : 1}
         onClick={(e) => setActive(!active)}
         onPointerOver={(e) => setHover(true)}
-        onPointerOut={(e) => setHover(false)}
-
-        >
-        <planeBufferGeometry attach="geometry" args={props.size} />
+        onPointerOut={(e) => setHover(false)}>
+        <planeBufferGeometry attach="geometry" args={size} />
         <meshStandardMaterial
           attach="material"
           map={imgTexture}
-          side={DoubleSide}
-        />
+          side={DoubleSide}/>
       </mesh>
       <Html 
-        position = {[props.position[0]-props.size[0], 1, 1]}
+        position = {[position[0]-4, position[1], 3]}
         as='div'
         className="description"
         style={{
@@ -45,32 +42,32 @@ function Exhibit(props) {
           pointerEvents: "none"
         }}>
           <img 
-            src={props.image} 
-            alt={props.text}
-            style={{maxWidth: "30vw"}}
+            src={image} 
+            alt={altText}
+            style={{maxWidth: "40vw"}}
             />
-          <h2>{props.text}</h2>
-          <p> {props.description}</p>
+          <h2>{text}</h2>
+          <p> {description}</p>
 
 
         </Html>
       <primitive object={spot} 
-        position={[props.position[0],5, props.museumParams[2]]} 
+        position={[position[0],5,museumParams[2]]} 
         intensity={0.8}
         penumbra={0.3}
         angle={0.6}
         />
-      <primitive object={spot.target} position={[props.position[0],0, 0]} />
+      <primitive object={spot.target} position={[position[0],0, 0]} />
       <Text
-        rotation = {props.rotation}
-        position={[props.position[0],props.position[1]-3, props.position[2]]}
+        // rotation = {rotation}
+        position={[position[0],position[1]-3, position[2]]}
         color={"white"}
         opacity={hovered ? 1 : 0}
         fontSize={hovered ? 0.25 : 0.2}
         anchorX="center"
         anchorY="middle"
         >
-        {`${props.text}`}
+        {`${text}`}
       </Text>
       
     </>
